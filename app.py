e5d2b3962877c3cef22278d1abf251aeb756d22b
ayanrahatali786 <ayanrahat54@gmail.com>
2024-01-22 01:29:56 +0530

    code added

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask,request,redirect,url_for,render_template
-
+from flask import Flask,request,redirect,url_for,render_template,jsonify
+import json
 app = Flask(__name__)
 
 
@@ -9,20 +9,20 @@ def welcome():
 
 @app.route('/cal', methods=["GET"])
 def math_operators():
-    operation= request.json[operation]
-    number1=request.json[number1]
-    number2=request.json[number2]
+    operation= request.json["operation"]
+    number1=request.json["number1"]
+    number2=request.json["number2"]
 
     if operation == "addition":
-        result=number1+number2
+        result=int(number1)+int(number2)
     elif operation=="subtraction":
-        result=number1-number2
+        result=int(number1)-int(number2)
     elif operation=="multiplication":
-        result=number1*number2
+        result=int(number1)*int(number2)
     else:
-        result=number1/number2
+        result=int(number1)/int(number2)
 
-    return result
+    return jsonify(result)
 
 if __name__ =="__main__":
     app.run(debug=True)
